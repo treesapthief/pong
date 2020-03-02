95d17044f19f3f1fae739336f2331c4904d5c334
Andrew McCauley <>
2020-03-01 21:46:38 -0600

    Adding goal triggers

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.gameObject.name == "Ball") {
+            Debug.Log("Point Scored");
+            Destroy(collision.collider.gameObject);
+        }
+    }
+}
