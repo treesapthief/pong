54d5ec912900e40ff46e73e737418d13df393223
Andrew McCauley <>
2020-03-01 20:57:22 -0600

    Initial commit of base pong "follow-along" tutorial

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.name == "RacketLeft") {
+            float y = hitFactor(transform.position, collision.transform.position,
+            collision.collider.bounds.size.y);
+            Vector2 dir = new Vector2(1, y).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        if (collision.gameObject.name == "RacketRight") { 
+            float y = hitFactor(transform.position,
+            collision.transform.position,
+            collision.collider.bounds.size.y);
+            Vector2 dir = new Vector2(-1, y).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight) {
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+}
